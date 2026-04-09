3e3689a96e4976711fbebc0fbd6062ff283da19c
SzymonSzot <s33692@pjwstk.edu.pl>
2026-04-09 08:55:50 +0200

    RoomsController added

---
+++ WebApplication1/Controllers/RoomsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        
+    }
+}

bd55f7b0209ede6065d62d602cfe55f924c002e2
Szymon Szot <s33692@pjwstk.edu.pl>
2026-04-14 18:03:35 +0200

    Models data structure implementation

---
+++ WebApplication1/Models/Reservation.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApplication1.Models;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Reservation
+{
+    private static int _id = 0;
+    
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    public string OrganizerName { get; set; }
+
+    public string Topic { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; }
+
+    public Reservation(int roomId, string organizerName, string topic, DateOnly date, TimeOnly startTime, TimeOnly endTime, string status )
+    {
+        Id = _id++;
+        RoomId = roomId;
+        OrganizerName = organizerName;
+        Topic = topic;
+        Date = date;
+        StartTime = startTime;
+        EndTime = endTime;
+        Status = status;
+    }
+
+}
--- WebApplication1/Models/Room.cs
+++ WebApplication1/Models/Room.cs
@@ -1,7 +1,10 @@
 ﻿namespace WebApplication1.Models;
 
 public class Room
+
 {
+    private static int _id = 0;
+    
     public int Id { get; set; }
     public string Name { get; set; }
     public string BuildingCode { get; set; }
@@ -9,5 +12,15 @@ public class Room
     public int Capacity { get; set; }
     public bool HasProjector { get; set; }
     public bool IsActive { get; set; }
-    
+
+    public Room(string name, string buildingCode, int floor, int capacity, bool hasProjector, bool isActive)
+    {
+        Id = _id++; 
+        Name = name;
+        BuildingCode = buildingCode;
+        Floor = floor;
+        Capacity = capacity;
+        HasProjector = hasProjector;
+        IsActive = true;
+    }
 }

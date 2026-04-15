2365261dd6f83af20885d2aea2256f0b04c84865
Szymon Szot <s33692@pjwstk.edu.pl>
2026-04-15 21:44:30 +0200

    Room data requirements

--- WebApplication1/Models/Room.cs
+++ WebApplication1/Models/Room.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApplication1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
 
 public class Room
 
@@ -6,10 +8,11 @@ public class Room
     private static int _id = 0;
     
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string BuildingCode { get; set; }
+    
+    [Required] public string Name { get; set; }
+    [Required] public string BuildingCode { get; set; }
     public int Floor { get; set; }
-    public int Capacity { get; set; }
+    [Range(1, int.MaxValue)] public int Capacity { get; set; }
     public bool HasProjector { get; set; }
     public bool IsActive { get; set; }
 
